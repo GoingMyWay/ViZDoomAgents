541fc6058355787c54939f2c8239cd183e4f889d
邱伟 <qiuwei@MacBook-Pro.local>
2017-07-26 12:07:36 +0800

    model path

--- healthpack_gathering/configs.py
+++ healthpack_gathering/configs.py
@@ -3,4 +3,4 @@
 img_dim = 80
 a_size = 3
 ACTION_DIM = 2 ** a_size - 2  # remove (True, True, True) and (True, True, False)[Left, Right, Forward]
-
+model_path = './healthpack_gathering/check_point'
