9bb30579ba739ebca6a91ca5a5ef8722731528d4
邱伟 <qiuwei@MacBook-Pro.local>
2017-08-26 19:07:07 +0800

    visualizer

--- battle/configs.py
+++ battle/configs.py
@@ -6,6 +6,7 @@ model_path = './check_point/D3_battle/'
 model_file = 'model-51000.ckpt'
 model_file = 'model-8700.ckpt'
 model_file = 'model-30150.ckpt'
+model_file = 'model-41050.ckpt'
 
 SCENARIO_PATH = '../scenarios/D3_battle.cfg'
 
@@ -37,3 +38,32 @@ SKIP_FRAME_NUM = 4
 
 AGENT_PREFIX = 'Agent_'
 AGENT_MONITOR = AGENT_PREFIX + '0'
+
+
+starter_entropy_rate = 0.1
+decay_steps = 1000
+decay_rate = 0.96
+
+
+left = 'images/left.png'
+left_back = 'images/left_back.png'
+
+right = 'images/right.png'
+right_back = 'images/right_back.png'
+
+forward = 'images/forward.png'
+forward_back = 'images/forward_back.png'
+
+turn_left = 'images/turn_left.png'
+turn_left_back = 'images/turn_left_back.png'
+
+turn_right = 'images/turn_right.png'
+turn_right_back = 'images/turn_right_back.png'
+
+attack = 'images/attack.png'
+attack_back = 'images/attack_back.png'
+
+speed = 'images/speed.png'
+speed_back = 'images/speed_back.png'
+
+MAX_TIME_OUT_STEP = 2100
