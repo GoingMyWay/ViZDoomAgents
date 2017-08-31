f22fefe4002b8b077eb304147f26c03d43780f72
邱伟 <qiuwei@MacBook-Pro.local>
2017-08-31 09:27:25 +0800

    update

--- healthpack_gathering/configs.py
+++ healthpack_gathering/configs.py
@@ -1,6 +1,13 @@
 # coding: utf-8
 
-img_dim = 80
+IMG_SHAPE = (80, 80)
 a_size = 3
 ACTION_DIM = 2 ** a_size - 2  # remove (True, True, True) and (True, True, False)[Left, Right, Forward]
-model_path = './healthpack_gathering/check_point'
+model_path = './check_point/supreme/'
+model_file = 'model-10000.ckpt'
+
+IS_SUPREME_VERSION = True
+IS_TRAIN = False
+AGENTS_NUM = 16
+
+HIST_LEN = 4
